a8dc9a8a94ad5aa0502a065b7f42f0f76bf70e96
Vikas Pal <144196870+vikas0304@users.noreply.github.com>
2024-09-14 14:52:35 +0530

    Git error

--- index.js
+++ index.js
@@ -1,40 +1,24 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
+const Note = require('./models/note')
 
 
 
 app.use(express.json())
-
 app.use(cors())
-
 app.use(express.static('dist'))
 
 
-
-let notes = [
-    {
-      id: "1",
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: "2",
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: "3",
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
   app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
 
   app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
   })
   
   app.get('/api/notes/:id', (request, response) => {
@@ -64,22 +48,18 @@ let notes = [
   app.post('/api/notes', (request, response) => {
     const body = request.body
   
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
   
-    const note = {
-      id: generateId(),
+    const note = new Note({
       content: body.content,
-      important: Boolean(body.important) || false,
-      
-    }
+      important: body.important || false,
+    })
   
-    notes = notes.concat(note)
-  
-    response.json(note)
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
   })
   
   const PORT = process.env.PORT || 3000
